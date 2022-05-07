253bf6057d0faaff70c9d3942df2ead34b5b695c
dahyeong_lim(임다형) <94767826+yattho@users.noreply.github.com>
2022-05-07 18:25:27 +0900

    Update Choose.java

--- src/line/Choose.java
+++ src/line/Choose.java
@@ -3,6 +3,23 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.*;
+import java.io.*;
+import java.util.*;
+
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.services.sheets.v4.Sheets;
+import com.google.api.services.sheets.v4.SheetsScopes;
+import com.google.api.services.sheets.v4.model.ValueRange;
 
 public class Choose extends  JFrame{
 	Container con = getContentPane();
@@ -12,13 +29,13 @@ public class Choose extends  JFrame{
 		Button listBtn;
 		Button searchBtn;
 	Choose(){
-        super("∞¸∏Æ¿⁄");
+        super("Í¥ÄÎ¶¨Ïûê");
         GridLayout gu = new GridLayout(3,2);
         setLayout(gu);
-        addBtn = new Button("µÓ∑œ");
-        deleteBtn = new Button("ªË¡¶");
-        listBtn = new Button("∏Ò∑œ");
-        searchBtn = new Button("∞Àªˆ");
+        addBtn = new Button("Îì±Î°ù");
+        deleteBtn = new Button("ÏÇ≠Ï†ú");
+        listBtn = new Button("Î™©Î°ù");
+        searchBtn = new Button("Í≤ÄÏÉâ");
             
         setLayout(new BoxLayout(con, BoxLayout.Y_AXIS));
         add(addBtn); 
@@ -67,7 +84,27 @@ public class Choose extends  JFrame{
         setVisible(true);
 		}
    
-	public static void main(String[] args) {
-        new Choose();
+	public static void main(String[] args) throws IOExeption {
+                  new Choose();      
+       Sheets service = getSheetsService(AuthMode.OAUTH20);
+       Sheets service = getSheetsService(AuthMode.SERVICE_ACCOUNT);
+
+       String spreadsheetId = "1A1SXyBEA4zfDuUdg6dN-tV52OENYmvnka9rv8ivp6xY";
+       
+       String range = "A3:A11";
+       ValueRange response = service.spreadsheets().values()
+                .get(spreadsheetId, range)
+                .execute();
+      List<List<Object>> values = response.getValues();
+      if (values == null || values.size() == 0) { 
+     	System.out.println("No data found."); 
+      } else { 
+      		for (List row : values) { 
+      			if (row.size() > 0) { 
+      				System.out.println(row.get(0).toString());
+      			}	
+      		}
+         }
+    
     }
 }
