41fc9fa5e7dd101a5e13b8b14570cfc55b9181e3
dahyeong_lim(임다형) <94767826+yattho@users.noreply.github.com>
2022-05-08 23:59:43 +0900

    Update Delete.java

--- src/line/Delete.java
+++ src/line/Delete.java
@@ -55,7 +55,27 @@ public class Delete extends  JFrame{
         setVisible(true);
 		}
    
-	public static void main(String[] args) {
-        new Delete();
+	public static void main(String[] args) throws IOExeption {
+	 new Delete();	
+	//    Sheets service = getSheetsService(AuthMode.OAUTH20);
+	Sheets service = getSheetsService(AuthMode.SERVICE_ACCOUNT);
+
+	String spreadsheetId = "1A1SXyBEA4zfDuUdg6dN-tV52OENYmvnka9rv8ivp6xY";
+
+	String range = "A3:A11";
+	ValueRange response = service.spreadsheets().values()
+		.get(spreadsheetId, range)
+		.execute();
+	List<List<Object>> values = response.getValues();
+	if (values == null || values.size() == 0) { 
+	System.out.println("No data found."); 
+	} else { 
+		for (List row : values) { 
+			if (row.size() > 0) { 
+				System.out.println(row.get(0).toString());
+			}	
+		}
+         }
+    
     }
 }
