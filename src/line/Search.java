4eae223f41f8b2b4b649468ac4247396f56b387f
dahyeong_lim(임다형) <94767826+yattho@users.noreply.github.com>
2022-05-09 00:02:19 +0900

    Update Search.java

--- src/line/Search.java
+++ src/line/Search.java
@@ -3,6 +3,23 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.*;
+import java.io.*;
+import java.util.*;
+
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.services.sheets.v4.Sheets;
+import com.google.api.services.sheets.v4.SheetsScopes;
+import com.google.api.services.sheets.v4.model.ValueRange;
 
 public class Search extends  JFrame{
     private static final long serialVersionUID = 1L;
@@ -11,12 +28,12 @@ public class Search extends  JFrame{
      Button searchBtn;
 
      Search(){
-        super("�˻�");
+        super("°Ë»ö");
         GridLayout gu = new GridLayout(3,2);
         setLayout(gu);
-        lblId = new Label("�ֹ��� ��ȣ");
+        lblId = new Label("ÁÖ¹®ÀÚ ¹øÈ£");
         txtId = new TextField();
-        searchBtn = new Button("Ȯ��");
+        searchBtn = new Button("È®ÀÎ");
         
         add(lblId);
         add(txtId);
@@ -41,7 +58,28 @@ public class Search extends  JFrame{
         setVisible(true);
 		}
    
-	public static void main(String[] args) {
-        new Search();
+
+	public static void main(String[] args) throws IOExeption {
+                   new Search();     
+   //    Sheets service = getSheetsService(AuthMode.OAUTH20);
+       Sheets service = getSheetsService(AuthMode.SERVICE_ACCOUNT);
+
+       String spreadsheetId = "1A1SXyBEA4zfDuUdg6dN-tV52OENYmvnka9rv8ivp6xY";
+       
+       String range = "A3:A11";
+       ValueRange response = service.spreadsheets().values()
+                .get(spreadsheetId, range)
+                .execute();
+      List<List<Object>> values = response.getValues();
+      if (values == null || values.size() == 0) { 
+     	System.out.println("No data found."); 
+      } else { 
+      		for (List row : values) { 
+      			if (row.size() > 0) { 
+      				System.out.println(row.get(0).toString());
+      			}	
+      		}
+         }
+    	
     }
 }
